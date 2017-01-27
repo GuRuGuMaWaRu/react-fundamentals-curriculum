5463645f03a606fde41cb1cfcb835770ca63c030
Peter Krevenets <peter.k.nets@gmail.com>
2017-01-27 09:59:35 +0200

    add styling to the Home component

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import '../styles/Home.css';
 
 class Home extends React.Component{
   render() {
     return (
-      <div>
-        <h1>Weather App</h1>
+      <div className="background flex">
+        <h1>Enter a City and State</h1>
+        <div className="col-sm-4 col-sm-offset-4">
+          <form onSubmit={null}>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="California, Los Angeles"/>
+            </div>
+            <div className="form-group col-sm-4 col-sm-offset-4">
+              <button
+                className="btn btn-block btn-success">
+                Get Weather
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
--- app/components/Main.js
+++ app/components/Main.js
@@ -4,8 +4,8 @@ import '../styles/Main.css';
 export default class Main extends React.Component{
   render() {
     return (
-      <div  className="background">
-        <h1>React Router Tutorial</h1>
+      <div className="main">
+        <h2>Main.js Header</h2>
         {this.props.children}
       </div>
     );
