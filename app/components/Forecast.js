00a20f7512da184f09f717a40be43f7a27b75a12
Peter Krevenets <peter.k.nets@gmail.com>
2017-02-02 12:24:40 +0200

    Create empty Forecast component

---
+++ app/components/Forecast.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Forecast extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Forecast Component</div>
+    )
+  }
+}
