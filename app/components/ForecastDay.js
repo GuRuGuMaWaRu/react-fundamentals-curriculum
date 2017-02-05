20af19cee1cecb5130ea76dd4e666e592c59ba15
Peter Krevenets <peter.k.nets@gmail.com>
2017-02-05 12:15:24 +0200

    Add utils file with helpers and extract getDate function into it

--- app/components/ForecastDay.js
+++ app/components/ForecastDay.js
@@ -1,22 +1,18 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
 
-function ForecastDay(props) {
-  const today = new Date();
-  const options = {weekday: 'long', month: 'short', day: 'numeric'};
+var getDate = require('../helpers/utils').getDate;
 
+function ForecastDay(props) {
   function onClick() {
     props.onSelectDay(props.day);
   }
-
-  today.setTime(props.day.dt * 1000);
-
   return (
     <div onClick={onClick}>
       <img
         src={'http://openweathermap.org/img/w/' + props.day.weather[0].icon + '.png'}
         style={props.styles} />
-      <p>{today.toLocaleDateString('en-US', options)}</p>
+      <p>{getDate(props.day.dt)}</p>
     </div>
   );
 }
